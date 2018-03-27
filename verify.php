ef5d7d661443a08122368234ee73b8fcb68c37aa
Jakgrits14 <37709056+Jakgrits14@users.noreply.github.com>
2018-03-27 12:01:37 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,7 @@
 <?php
-$access_token = 'XKWmuwJDepwPa8/zCNYTj2tNzLGFLpHpVTropgUv94AOMP9s6I7Uo4dL8L+96jMO3BwctdLUEIjge+Labpcohi+gb8YaHbJFuAx2Jc2XTahUmNjDbf+9VblQs5p/XwLocml0HS2Fj8MFCA9FN8kfgAdB04t89/1O/w1cDnyilFU=';
-$url = 'https://api.line.me/v1/oauth/verify';$headers = array('Authorization: Bearer ' . $access_token);$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);$result = curl_exec($ch);curl_close($ch);echo $result;
+$access_token = 'fMcjIQwxtTaSpnukO+x2A9tWXzq5pYWL73lfItfkrYSd2G0+i04XbiHa7wdlBuOG3BwctdLUEIjge+Labpcohi+gb8YaHbJFuAx2Jc2XTajdUHOzg01TCZDko8TPrVZ6mBiV06JlmbYaWD2dsptItAdB04t89/1O/w1cDnyilFU=';
+$url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);curl_close($ch);
+echo $result;
